2da4ea6bcc97865f2fa9c1704f949e157fdcb971
k156 <jm_91@live.co.kr>
2019-03-06 13:12:05 +0900

    ddd

--- fibonacci.R
+++ fibonacci.R
@@ -1,8 +1,17 @@
-fibonacci = function(x) {
-  x_1 <- 
-  x_2 
-  print(x_2 + x_1)
+fibonacci = function(num) {
+  while(TRUE) {
+    x_2 <- num - 
+    x_1 <- num - x_2
+    x = x_1 + x_2
+  print(x)
+  if (x <= 1) {break}
+  }
 }
 
+#  x_1 = 
+  # x_2 = num - x_1
+
+
+fibonacci(13)
 
 (0 1 1 2 3 5 8 13)
--- trythis5.R
+++ trythis5.R
@@ -1,5 +1,8 @@
+#1
 data$group = sample(c('A조','B조','C조'), size= 160, replace=T)
 
+
+#2
 smdt$Math = as.numeric(smdt$Math)
 smdt$Korean = as.numeric(smdt$Korean)
 smdt$English = as.numeric(smdt$English)
@@ -15,5 +18,10 @@ smdt = cbind(smdt, avg =avg)
 
 
 
-for (i in 2016:2019) 
-  for (j in month.abb) {print( i-j)}
+
+#3
+dfsum = cbind( data.frame(no=1:4, year=2016:2019), 
+               matrix(round(runif(n=48, min=5, max=9)*10000 )  , ncol=12, dimnames = list(NULL, month.abb) )     ) 
+dfsum
+meltsum = melt(data = dfsum[,2:13] , id.vars = 'year', variable.name = 'month', value.name = 'saleamt')
+meltsum
